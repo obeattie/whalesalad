990eaafaf3d6338a92bc0d38b48239a3cc22db8e
Michael Whalen <michael@jawbreaker.local>
2008-08-03 07:55:16 -1000

    Testing this out...

--- urls.py
+++ urls.py
@@ -5,6 +5,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^$', 'django.views.generic.simple.direct_to_template', { 'template' : 'home.html' }),
+    (r'^about/$', 'django.views.generic.simple.direct_to_template', { 'template' : 'about.html' }),
+
     (r'^', include('whalesalad.blog.urls')),
     
     # Django admin shiz
