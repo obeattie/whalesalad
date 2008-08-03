f0a095f3c320917a68fc96677c9b35e73acbcab4
Oliver Beattie <Oliver@oliver-beatties-imac.local>
2008-08-03 17:35:38 +0100

    Update :(

--- local_settings.py
+++
@@ -1,6 +0,0 @@
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'django_whalesalad'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'django'             # Not used with sqlite3.
-DATABASE_PASSWORD = '(ziaZoat0()_+'         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
