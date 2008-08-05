d670fbddc94d252b5199da49afb4fa96da9e2f6c
Oliver Beattie <Oliver@oliver-beatties-imac.local>
2008-08-05 10:43:53 +0100

    Fixed migration script to work with whaledb

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -3,4 +3,4 @@ from whalesalad.blog.models import Post
 
 def add_recent_posts(request):
     """Adds the 5 most recent posts to a recent_posts context variable."""
-    return { 'recent_posts': Post.objects.get_public().order_by('-published')[:5] }
+    return { 'recent_posts': Post.objects.get_published().order_by('-published')[:5] }
--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 
 from whalesalad.blog import redirects as blog_redirects, views as blog_views
+from whalesalad.misc import views as misc_views
 
 # Admin-site autodiscovery shiz
 admin.autodiscover()
@@ -15,10 +16,12 @@ urlpatterns = patterns('',
     
     # Miscellaneous URLs and stuff
     url(r'^/?$', blog_views.index, name='home'),
-    url(r'^about/$', 'django.views.generic.simple.direct_to_template', { 'template' : 'static/about.html' }),
-    url(r'^elixir/$', 'django.views.generic.simple.direct_to_template', { 'template' : 'static/elixir.html' }),
+    url(r'^about/$', misc_views.about, name='about'),
+    
+    # Static pages
+    url(r'^elixir/$', 'django.views.generic.simple.direct_to_template', { 'template': 'static/elixir.html' }),
     # Contact as static for now until its dynamic :D
-    url(r'^contact/$', 'django.views.generic.simple.direct_to_template', { 'template' : 'static/contact.html' }),
+    url(r'^contact/$', 'django.views.generic.simple.direct_to_template', { 'template': 'static/contact.html' }),
     
     # Django admin shiz
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
